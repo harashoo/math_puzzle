a3a3799d4bb36ff4e97c7f0e4928965c65276349
shogo-hr <harada.tosho.2016@gmail.com>
2020-01-18 12:13:01 +0900

    fix

--- q5.rb
+++ q5.rb
@@ -3,10 +3,15 @@ N = 15
 input = 1000
 
 (0..N).each do |i|
-  (0..N - i).each do |j|
-    (0..N - i - j).each do |k|
-      m = N - i - j - k
-      count += 1 if input == 500 * i + 100 * j + 50 * k + 10 * m
+  (0..N).each do |j|
+    (0..N).each do |k|
+      (0..N).each do |m|
+        if N >= i + j + k + m
+          if input == 500 * i + 100 * j + 50 * k + 10 * m
+            count += 1
+          end
+        end
+      end
     end
   end
 end
