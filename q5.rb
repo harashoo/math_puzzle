9af23575a735246c1b0daa230654d51b55e7c8a7
shogo-hr <harada.tosho.2016@gmail.com>
2020-01-18 11:55:01 +0900

    add variable

--- q5.rb
+++ q5.rb
@@ -1,11 +1,12 @@
 count = 0
 N = 15
+input = 1000
 
 (0..N).each do |i|
   (0..N - i).each do |j|
     (0..N - i - j).each do |k|
       m = N - i - j - k
-      count += 1 if 1000 == 500 * i + 100 * j + 50 * k + 10 * m
+      count += 1 if input == 500 * i + 100 * j + 50 * k + 10 * m
     end
   end
 end
